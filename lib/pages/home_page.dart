b1638a81c3014b8b60f06b2682389102887ecead
Bruno Rizzo <Bruno25.rs@gmail.com>
2020-04-08 15:10:51 -0300

    primeiro commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Future<Map> _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+    http.Response response = await http.get(url);
+    return json.decode( response.body );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot){
+
+        String resultado;
+        switch( snapshot.connectionState ){
+          case ConnectionState.none :
+          case ConnectionState.waiting :
+            print("conexao waiting");
+            resultado = "Carregando...";
+            break;
+          case ConnectionState.active :
+          case ConnectionState.done :
+            print("conexao done");
+            if( snapshot.hasError ){
+              resultado = "Erro ao carregar os dados.";
+            }else {
+
+              double valor = snapshot.data["BRL"]["buy"];
+              resultado = "Preço do bitcoin: ${valor.toString()} ";
+
+            }
+            break;
+        }
+
+        return Scaffold(
+          body: Container(
+            padding: EdgeInsets.only(top:70),
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  resultado,
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                  ),
+              ],
+            ),
+          ),
+        );
+
+      },
+    );
+  }
+}
